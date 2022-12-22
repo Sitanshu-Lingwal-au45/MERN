6b8579efa29278d7cc422b33b7d1e7857f083c28
Sitanahu-Lingwal-au45 <slslsingh17@gmail.com>
2022-12-22 11:07:43 +0530

    commit 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,30 @@
-import './App.css';
-import Header from "./component/layout/Header.js"
-import {BrowserRouter as Router} from "react-router-dom"
-import webfont from "webfontloader"
-import React from 'react';
+import "./App.css"
+import Header from "./component/layout/Header/Header.js"
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import webFont from "webfontloader"
+import React from "react";
+import Footer from "./component/layout/Footer/Footer.js"
+import Home from "./component/Home/Home.js"
 
-React.useEffect(())
 function App() {
-  return ( 
-        <Router>
-         <Header />
-        </Router>
-     );
+  React.useEffect(()=>{
+    webFont.load({
+      google:{
+        families: ["Roboto","Droid Sans","Chilanka"]
+      }
+    })
+  },[]) 
+  
+  return (
+    <div>
+    <Router>
+    <Header/>
+          <Route exact path="/" component={Home}/>
+      
+    <Footer/>
+    </Router>
+    </div>
+);
 }
 
 export default App;
